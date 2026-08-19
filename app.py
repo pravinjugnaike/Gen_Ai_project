8a81e290fd5b1525fd39600e36f74aa45237957d
Pravin Deoram Jugnaike <pravinjugnaike2004@gmail.com>
2026-08-19 16:12:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+#creating my prompts 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant, please to the questions"),
+        ("user","Question:{question}")
+    ]
+)
+
+#frontend using streamlit
+st.title("Chat GPT")
+input_text = st.text_input("Ask your questions !")
+
+#Ollama and LLM model integration
+llm = Ollama(model="gemma2:2b")
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text})) 
